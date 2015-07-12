ab77c4f655a619cc8b77cfc0b6923f6d46bcf492
Jason Edelstein <jasone@uchicago.edu>
2015-07-12 00:40:36 -0500

    Swiggity swooty! I've commited the code and such to finish this homework. I think.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-		@places = Place.all
+	@places = Place.all
   end
   
   def show
@@ -13,5 +13,30 @@ class PlacesController < ApplicationController
 	place.delete
 	redirect_to root_url
   end
+  
+  def new
+  end
+  
+  def edit
+	@place = Place.find_by(:id => params["id"])
+  end
+  
+  def update
+	@place = Place.find_by(:id => params["id"])
+	@place.title = params["title"]
+	@place.photo_url = params["photo_url"]
+	@place.price = params["price"]
+	@place.description = params["description"]
+	@place.save
+	redirect_to "/places/#{@place.id}"
+  end
+  
+  def create
+	place = Place.new(:title => params["title"], :photo_url => params["photo_url"], :price => params["price"], :description => params["description"])
+	place.save
+	redirect_to root_url
+  end
+  
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
 
 	root 'places#index'
 	
+	get '/places/new' => 'places#new'
+	
+	get '/places/create' => 'places#create'
+	
+	get '/places/:id/edit' => 'places#edit'
+	
+	get '/places/:id/update' => 'places#update'
+	
 	get '/places' => 'places#index'
 	
 	get '/places/:id' => 'places#show'
